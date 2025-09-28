4b6aa59ff8b45561b48212736d0f747f56e5bcc9
Víctor Pagola <victorpagola.w@gmail.com>
2025-09-28 11:06:17 +0200

    new: proyect done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.printf("| %5s | %5s | %5s | %5s | %5s |%n",
+        "Lunes", "Martes", "Miérc.", "Jueves", "Viernes");
+        System.out.println("══════════════════════════════════════════════");
+        System.out.printf("| %-5s | %-6s | %-6s | %-6s | %-7s |%n",
+        "PROG", "PROG", "PROG", "PROG", "SIN");
+        System.out.printf("| %-5s | %-6s | %-6s | %-6s | %-7s |%n",
+        "PROG", "PROG", "PROG", "PROG", "SIN");
+        System.out.printf("| %-5s | %-6s | %-6s | %-6s | %-7s |%n",
+        "ED", "SIN", "SIN", "LM", "BDATO");
+        System.out.printf("| %-5s | %-6s | %-6s | %-6s | %-7s |%n",
+        "FOL", "SIN", "SIN", "LM", "BDATO");
+        System.out.printf("| %-5s | %-6s | %-6s | %-6s | %-7s |%n",
+        "FOL", "BDATO", "ED", "BDATO", "ED");
+        System.out.printf("| %-5s | %-6s | %-6s | %-6s | %-7s |%n",
+        "FOL", "BDATO", "ED", "BDATO", "ED");
     }
 }
